5f3b5d41cb24bae653804f3e924a29e909d09862
jeremy perry <jeremy.m.perry@gmail.com>
2019-08-30 20:33:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+
 function dwarfRollCall(dwarves) {
+  var newDwarfs = [];
+  for (var i = 0; i < dwarves.length; i++){
+    newDwarfs.push(`${i + 1}. ${dwarves[i]} `);
+  }
+  return newDwarfs.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var array = [];
+  for (let i=0; i<planeteerCalls.length; i++){
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase(); 
+    array.push(`${planeteerCalls[i]}!`);
+  }
+  return array;
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length<=4){
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i=0; i<foods.length; i++){
+    foods.shift();
+    if (foods[i] === "gouda"){
+      return `gouda`;
+    }
+    else if (foods[i] === `cheddar`){
+      return 'cheddar';
+    }
+    else {
+      return `no cheese!`;
+      }
+  }
 }
